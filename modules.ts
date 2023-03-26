c06edcb074612d32d4c6da4f212d94d5dff7144d
Mathys <mathystheolade973@gmail.com>
2023-03-26 13:09:32 +0200

    switch json to js

---
+++ modules.ts
@@ -0,0 +1,8 @@
+export const modules = [
+    {
+        name: "mon_premier_module",
+        path: "F:/Dev/network/core/module-cors/example/module-2",
+        loadAsConfiguration: true,
+        version: "0.0.1",
+    }
+]
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 // import a json file using esnext module syntax
-import modulesToLoad from '../modules.json';
+import { modules } from '../modules';
 import { ApplicationFactory } from './core/module-factory';
 
 async function bootstrap() {
   const app = new ApplicationFactory();
-  await app.create(modulesToLoad);
+  await app.create(modules);
   await app.init();
 }
 bootstrap();
