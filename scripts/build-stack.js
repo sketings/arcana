9b815552af8cc64aa2542757c053f5efbd93488f
Mathys <mathystheolade973@gmail.com>
2023-04-16 19:09:53 +0200

    wip/ add build stack script

---
+++ scripts/build-stack.js
@@ -0,0 +1,4 @@
+// Lancer le script nginx
+// Ensuite lancer le script de génération du Dockerfile
+// Ensuite lancer le script de génération de docker-compose
+// Génerer un rapport des routes qui ont été générées
