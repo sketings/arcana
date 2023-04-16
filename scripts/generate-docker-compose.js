723d1422b04ae9249504439cc29df1f60ddc43c7
Mathys <mathystheolade973@gmail.com>
2023-04-16 18:35:39 +0200

    add docker compose script

--- scripts/generate-docker-compose.js
+++ scripts/generate-docker-compose.js
@@ -1,19 +1,76 @@
-const modules = require('./modules.json');
+const fs = require('fs');
+const path = require('path');
 
-const services = {};
+// Get list of modules
+const modulesFile = path.join(__dirname, '../modules.json');
+const modules = JSON.parse(fs.readFileSync(modulesFile));
 
-modules.forEach((module) => {
-  services[module.name] = {
-    image: module.dockerImage,
-    ports: [`${module.port}:${module.port}`],
-    // autres options pour chaque module
-  };
-});
 
-const composeFile = {
-  version: '3',
-  services,
-  // autres options pour le fichier docker-compose.yml
-};
+let moduleVolume = '';
+let modulePort = '';
+let linkApp = '';
+let isolatedService = '';
+const escapeNumber = 6;
 
-console.log(JSON.stringify(composeFile, null, 2)); 
+for (const module of modules) {
+  const tempModuleVolume = `- ./integration/nginx/${module.name}.conf:/etc/nginx/conf.d/${module.name}.conf \n`;
+  moduleVolume += ' '.repeat(escapeNumber) + tempModuleVolume;
+  if(module.port){
+    const tempModulePort = `- "${module.port}:${module.port}" \n`;
+    modulePort += ' '.repeat(escapeNumber) + tempModulePort;
+  }
+
+  if(module.isolated){
+    linkApp += ' '.repeat(escapeNumber) + `- ${module.name} \n`;
+    isolatedService += `  
+  ${module.name}:
+    build:
+      context: ./modules/${module.folderName}
+      dockerfile: ./Dockerfile-${module.name}
+    environment:
+      - ENV=PRODUCTION
+      # Ajouter la possiblité de passer des variables d'environnement
+    ports:
+      - "${module.port}:${module.port}"
+    `;
+  }
+}
+
+const dockerComposeContent = `
+version: '3.1'
+
+services:
+  reverse-proxy:
+    image: nginx:latest
+    container_name: reverse-proxy
+    volumes:
+      - ./integration/nginx/core.conf:/etc/nginx/conf.d/core.conf
+      - ./integration/nginx/proxy.conf:/etc/nginx/conf.d/proxy.conf
+      - ./integration/nginx/html:/var/www/html
+      # je dois créer une conf nginx pour chaque module
+${moduleVolume}
+    ports:
+      - "80:80"
+      - "443:443"
+${modulePort}
+    links:
+      - app
+${linkApp}
+      
+      # - arcana_fastify
+      # - mon_premier_module
+  # donner la possibilite dans le script de création d'injecter des variables d'environnement
+  # TODO: pour chaque module qui sera ajouté séparement il faudra lui générer un dockerfile spécifique
+  app:
+    build:
+      context: ./
+      dockerfile: ./Dockerfile
+    environment:
+     - ENV=PRODUCTION
+${isolatedService}
+`;
+
+// Write Docker compose file
+fs.writeFileSync(path.join(__dirname, '../docker-compose.yml'), dockerComposeContent);
+
+console.log('Docker compose generated successfully');
