25deca21a54f7ab3da0bce11341abdae70e0cade
Mathys <mathystheolade973@gmail.com>
2023-05-28 16:32:57 +0200

    type app, add shared event

--- modules/arcana-fastify/loader.ts
+++ modules/arcana-fastify/loader.ts
@@ -1,24 +1,26 @@
 import Fastify from "fastify";
+import { Module } from "src/core/module";
 
 export default class App {
 
-    async start(app: any){
-        const server = Fastify({ logger: true });
-        server.get('/ping', async (request, reply) => {
-            return 'pong\n'
-          })
-          
-          server.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
-            if (err) {
-              console.error(err)
-              process.exit(1)
-            }
-            console.log(`Server listening at ${address}`)
-          })
-        
-    }
+  async start(app: Module) {
+    const cc = app.event.resolve('mon_premier_module:event', app);
+    const server = Fastify({ logger: true });
+    server.get('/ping', async (request, reply) => {
+      return 'pong\n'
+    })
 
-    async stop(){
-        
-    }
+    server.listen({ port: app.moduleConf.port ?? 3001, host: '0.0.0.0' }, (err, address) => {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+      console.log(`Server listening at ${address}`)
+    })
+
+  }
+
+  async stop() {
+
+  }
 }
--- modules/mon_premier_module/loader.ts
+++ modules/mon_premier_module/loader.ts
@@ -1,25 +1,26 @@
-
+import { Module } from "../../src/core/module";
 export default class App {
 
-    async start(app: any){
+    async start(app: Module){
+        console.log(app.moduleConf.name)
+        console.log('app.moduleConf.name')
         function numberValidator(nb: number){
             return isNaN(nb);
         }
-        
         app.setState('monState', 'Coucou')
-        app._event.subscribe('mon_premier_module:event', () => console.log('Hello world'), app, false);
-        app._event.subscribe('mon_premier_module:event_deux', new Test(), app, true);
-        app._event.subscribe('mon_premier_module:validddd', numberValidator, app, true);
+        app.event.subscribe('mon_premier_module:event', () => console.log('Hello world'), app, false);
+        app.event.subscribe('mon_premier_module:event_deux', new Test(), app, true);
+        app.event.subscribe('mon_premier_module:validddd', numberValidator, app, true);
 
-        const { cb } = app._event.resolve('system:module_manager', app);
+        const { cb } = app.event.resolve('system:module_manager', app);
         app.setAppState('leStateDeLapp', 'Coucou je suis l application')
         const appState = app.getAppState('leStateDeLapp')
         // console.log(appState);
         const availaible_module = cb.getModulesAvailableName();
         
-        const cc = app._event.resolve('mon_premier_module:event_deux', app);
-        const ffff = app._event.resolve('mon_premier_module:validddd', app, 'fg');
-        console.log(app._event);
+        // const cc = app.event.resolve('mon_premier_module:event_deux', app);
+        const ffff = app.event.resolve('mon_premier_module:validddd', app, 'fg');
+        console.log(app.event);
         console.log(ffff);
         // console.log(cc.cb.init());
         // logger.cb.log('Hello world');
